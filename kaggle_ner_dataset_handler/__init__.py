159fe1e63fda8686122dae4e4dba3d5225e79395
lovit <soy.lovit@gmail.com>
2020-05-11 08:54:04 +0900

    Initialize

---
+++ kaggle_ner_dataset_handler/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'lovit'
+__version__ = '0.1.0'
+
+from .handler import installpath
+from .handler import load
+
---
+++ kaggle_ner_dataset_handler/handler.py
@@ -0,0 +1,9 @@
+import os
+
+
+installpath = os.path.abspath(os.path.dirname(__file__))
+
+
+def load(random_seed=42):
+    raise NotImplementedError
+
